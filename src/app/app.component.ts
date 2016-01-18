d49c58dd715fdca063f17a414c236c6a7bc1ddd9
Iurii Kyrylenko <iurii_kyrylenko@epam.com>
2016-01-18 20:33:38 +0200

    Added canvas to the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,21 @@ import {FDraw} from './fdraw/fdraw.component';
     selector: 'app',
     directives: [FDraw],
     template: `
+        <input [(ngModel)]="params.width" />
+        ×
+        <input [(ngModel)]="params.height" />
+
+        {{params | json}}
+
         <fdraw [params]="params"></fdraw>
-        <fdraw [params]="{width: 1, height: 2, x: 3, y: 4, zoom: 5}"></fdraw>
         `
 })
 export class App {
     params: IFDrawParams = {
-        width: 400,
-        height: 300,
-        x: 0,
+        width: 340,
+        height: 540,
+        x: -0.6,
         y: 0,
-        zoom: 1
+        zoom: 100
     };
 }
--- src/app/fdraw/fdraw.component.ts
+++ src/app/fdraw/fdraw.component.ts
@@ -1,27 +1,41 @@
-import {Component, Input, ElementRef} from 'angular2/core';
+import {Component, Input, ElementRef, AfterViewInit} from 'angular2/core';
 import {Observable} from 'rxjs/Rx';
 import {IFDrawParams} from './fdraw-params';
 
 @Component({
     selector: 'fdraw',
+    styles: [`
+        canvas {
+            display: block;
+            border:1px solid #d3d3d3;
+        }
+    `],
     template: `
-        <div>
-            {{params | json}}
-        </div>
+        <canvas
+            [width]="params.width"
+            [height]="params.height">
+        </canvas>
     `
 })
-export class FDraw {
+export class FDraw implements AfterViewInit {
     @Input() params: IFDrawParams;
+    private _canvas: any;
 
     constructor(element: ElementRef) {
-        let targetElement = element.nativeElement;
-        console.log('targetElement:', targetElement);
+        this._canvas = element.nativeElement.querySelector('canvas');
+        console.log('canvas:', this._canvas);
 
-        let obs = Observable.fromEvent(targetElement, 'mousedown');
+        let obs = Observable.fromEvent(this._canvas, 'mousedown');
 
         obs.subscribe((event: MouseEvent) => {
-            console.log('event:', event);
             this.params.x = event.clientX;
+            this.params.y = event.clientY;
         });
+    }
+
+    ngAfterViewInit() {
+        let ctx = this._canvas.getContext('2d');
+        ctx.font = '30px Arial';
+        ctx.fillText('TEST', 10, 50);
     }
 }
